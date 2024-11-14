f3ae6d7a9eca116c4c5773193452fb7418ce33de
sudheerlankapalli <sudheerlankapalli55@gmail.com>
2024-11-14 23:18:40 +0530

    added txt

---
+++ src/main/java/com/example/coding/JavacodingApplication.java
@@ -0,0 +1,35 @@
+package com.example.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+
+@SpringBootApplication
+public class JavacodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacodingApplication.class, args);
+		int a=12;
+		String name="KRISHNA";
+		StringBuilder sb =new StringBuilder(name);
+		System.out.println(sb.length());
+	System.out.println("Name is " +name);
+	 
+	 Scanner sc= new Scanner(System.in);
+	 
+	 System.out.println("Enter the first number");
+	 int number1=sc.nextInt();
+	 
+	 System.out.println("Enter the second number");
+	 int number2=sc.nextInt();
+	 
+	 System.out.println("Addition " +(number1+number2));
+	 System.out.println("Subtraction "+(number1-number2));
+	 System.out.println("Multiplication"+(number1*number2));
+	 System.out.println("Division"+(number1/number2));
+	 System.out.println("Reminder"+(number1%number2));
+	 System.out.println("Code form the sudheer");
+	 //MASTER BRANCH
+	}
+
+}
